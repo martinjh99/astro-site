8617c4341a2638f18d5d2012aba6e222836d346c
Martin Hooper <martin@mjhooper.co.uk>
2024-12-28 15:55:47 +0000

    Changed flickr api call to use .env file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig,envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  env:{
+    schema:{
+      PUBLIC_FLICKR_API_KEY: envField.string({
+        context: "client",access: "public"
+      }),
+    }
+  }
 });
